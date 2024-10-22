62ad59ecc5af69ec069fddeca5dcee2d87d7df4a
David Ramirez <daver3754@gmail.com>
2024-10-22 15:20:52 -0500

    Add files via upload

---
+++ P1DR.py
@@ -0,0 +1,12 @@
+#David Ramirez
+#10/22/2024
+
+user1 = input("Say hello: ")
+
+if user1 == "hello" or "Hello":
+    for g in range(100):
+        print("hellooooooo")
+else:
+    print("Good bye")
+
+#This program says "hellooooooo" 100 times if the user says "hello" or "Hello"
---
+++ P2DR.py
@@ -0,0 +1,11 @@
+# David Ramirez
+# 10/22/2024
+
+numbers = [45, 94, 26, 90, 50, 84, 51, 68, 38, 34]
+
+for number in numbers:
+    if number%2 == 0:
+        print(f"Hey - {number} is even!")
+    else:
+        print(f"Gosh, {number} is odd!")
+#Uses the random numbers and divides them to check if they're even or odd (if the number reaches zero it's even, odd if it has one amount left)
